a94d59a53d30d6a25286da0a748ff39d25028430
zethra <jediben97@gmail.com>
2015-02-12 18:32:37 -0500

    Playing around with javascript autosizing tables

--- GUI/src/gui/gui.js
+++ GUI/src/gui/gui.js
@@ -1,6 +1,10 @@
 $(document).ready(function(e) {
-    $(this).has('input').addClass('check');
+	//$('.auto1').css('width', window.innerWidth * .9); 
 });
+
+window.onresize = function() {
+	//$('.auto1').css('width', window.innerWidth * .9); 
+}
 
 $('.check').click(function(e) {
     alert('clicked');
